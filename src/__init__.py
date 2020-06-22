b6f426b9a40dc11e97281fe7d134ce300ed5405b
Sambit Giri <sambitgiri@Sambits-MacBook-Pro.local>
2020-06-22 14:16:38 +0200

    first scripts emulation methods

--- src/__init__.py
+++ src/__init__.py
@@ -14,6 +14,7 @@ from . import sample_models
 from . import distances
 from . import bayesian_optimisation
 from . import corner
+from . import sampling_space
 from .emulate import *
 
 #Suppress warnings from zero-divisions and nans
